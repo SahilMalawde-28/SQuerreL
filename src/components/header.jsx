b592cd9139d720ce1c407eaf7531d3cfa6ae1f9c
shevkar-shubhra <161275088+shevkar-shubhra@users.noreply.github.com>
2025-03-17 16:24:01 +0530

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -10,10 +10,6 @@ export default function Header({ onSchemaInspectorToggle }) {
       <div className="flex items-center">
         <div className="text-green-500 mr-2">
             <img src={squerrelLogo} alt="Description" width="24" height="24" />
-            <path d="M8 9h8"></path>
-            <path d="M8 13h6"></path>
-            <path d="M18 4H6a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2Z"></path>
-          </svg>
         </div>
         <h1 className={cn("text-green-500 font-bold text-xl")}>SQuerreL</h1>
         <div className="flex items-center ml-4">
