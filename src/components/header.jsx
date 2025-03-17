17dbe9cbbd375366223f12ade3ec50e5ac59fa52
shevkar-shubhra <161275088+shevkar-shubhra@users.noreply.github.com>
2025-03-17 16:14:46 +0530

    Logo change

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -8,17 +8,7 @@ export default function Header({ onSchemaInspectorToggle }) {
     <header className="flex items-center justify-between px-4 py-2 border-b border-gray-800">
       <div className="flex items-center">
         <div className="text-green-500 mr-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
+            <img src="../public/squerrel logo.png" alt="Description" width="24" height="24" />
             <path d="M8 9h8"></path>
             <path d="M8 13h6"></path>
             <path d="M18 4H6a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2Z"></path>
