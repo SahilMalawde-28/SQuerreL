55d8f4dcd2a8e82d30394fac42b3c6f2f17093a3
Sahil Malawde <sahilmalawde49@gmail.com>
2025-03-18 21:43:52 +0530

    Update Pricing.jsx

--- src/app/Pricing.jsx
+++ src/app/Pricing.jsx
@@ -1,11 +1,11 @@
 import PricingPage from "../components/PricingPage"
-import Navbar from "../components/navbar"
+
 
 function Pricing() {
   return (
-    <Navbar />
+    
     <PricingPage />
-  );
+  )
 }
 
 export default Pricing
