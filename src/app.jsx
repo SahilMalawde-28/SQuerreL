858f926a06919bd1f9d8b03b3f7827edcc134c66
Sahil Malawde <sahilmalawde49@gmail.com>
2025-03-18 21:32:29 +0530

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Page from "./app/page";
 import LandingPage from "./app/LandingPage";
 import Pricing from "./app/Pricing";
+import Navbar from "./components/Navbar";
 
 const router = createBrowserRouter([
   { path: "/", element: <LandingPage /> },
