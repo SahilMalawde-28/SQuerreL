9cc3f32db5aed236f8bf4b6f0ee65ba98c1447a1
shevkar-shubhra <shubhrashevkar@gmail.com>
2025-03-19 10:35:56 +0530

    Consistency

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -9,7 +9,7 @@ export default function Header({ onSchemaInspectorToggle }) {
     <header className="flex items-center justify-between px-4 py-2 border-b border-gray-800">
       <div className="flex items-center">
         <div className="text-green-500 mr-2">
-            <img src={squerrelLogo} alt="Description" width="24" height="24" />
+          <img src={squerrelLogo} alt="SQuerrel Logo" className="w-8 h-8" />
         </div>
         <h1 className={cn("text-green-500 font-bold text-xl")}>SQuerreL</h1>
         <div className="flex items-center ml-4">
